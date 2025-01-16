cbbe603f8732d67e89acae54b9d79292526404a2
MyBeautyHusky <alex.fan015@gmail.com>
2025-01-16 20:28:00 +0100

    Creando la función de conectar a la base de datos para la base de datos de alumnos
    funcion de conectar con comentarios explicando cada línea de código, además de importar la librería de sqLite

--- ejercicioAlumnos.py
+++ ejercicioAlumnos.py
@@ -1,3 +1,13 @@
+import sqlite3
+
+def conectar():
+    #   Establecemos la conexión con la base de datos SQLite con nombre de 'alumnos.db'
+    con = sqlite3.connect('alumnos.db') #   Si la base de datos no existe, la crea
+    cursor = con.cursor()   #   Creamos un objeto llamado cursor para poder ejecutar comandos SQL
+    #   Devolvemos tanto la conexión como el cursor
+    return con, cursor  # Esto nos permite usar ambos objetos cuando sea necesario
+
+
 def menu():
     while True:
         print("""
